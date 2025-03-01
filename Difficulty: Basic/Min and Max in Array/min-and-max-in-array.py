3bd908061195b4b06a8dc1523f2b00b5144e8e17
RM Muzamil <50529410+rm-muzamil@users.noreply.github.com>
2025-03-01 13:45:42 +0500

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Min and Max in Array/min-and-max-in-array.py
@@ -0,0 +1,33 @@
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+# } Driver Code Ends
+
+#User function Template for python3
+#User function Template for python3
+
+class Solution:
+    def get_min_max(self, arr):
+        min_num = min(arr)
+        max_num = max(arr)
+        return min_num, max_num
+    
+
+
+#{ 
+ # Driver Code Starts.
+#Initial Template for Python 3
+
+if __name__ == "__main__":
+    t = int(input())
+    while t > 0:
+        arr = list(map(int, input().split()))
+        ob = Solution()
+        mn, mx = ob.get_min_max(arr)
+        print(mn, mx)
+        t -= 1
+        print("~")
+
+
+# } Driver Code Ends
