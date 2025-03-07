5b9ac778230410b713d20f1fae42baf7052e1259
RM Muzamil <50529410+rm-muzamil@users.noreply.github.com>
2025-03-07 22:20:59 +0500

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Check Equal Arrays/check-equal-arrays.py
@@ -0,0 +1,28 @@
+# User function Template for python3
+
+class Solution:
+    # Function to check if two arrays are equal or not.
+    def checkEqual(self, a, b) -> bool:
+        a = sorted(a)
+        b = sorted(b)
+        
+        if a == b:
+            return True
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+    t = int(input())
+    for tc in range(t):
+        arr1 = [int(x) for x in input().replace('  ', ' ').strip().split(' ')]
+        arr2 = [int(x) for x in input().replace('  ', ' ').strip().split(' ')]
+        ob = Solution()
+        if ob.checkEqual(arr1, arr2):
+            print("true")
+        else:
+            print("false")
+        print("~")
+# } Driver Code Ends
