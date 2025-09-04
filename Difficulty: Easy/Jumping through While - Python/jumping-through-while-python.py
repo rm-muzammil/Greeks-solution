f9d15a60880f7cb66e37e88552c4fcf1a08b85bf
RM Muzammil <50529410+rm-muzammil@users.noreply.github.com>
2025-09-04 20:35:22 +0500

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Jumping through While - Python/jumping-through-while-python.py
@@ -0,0 +1,11 @@
+def printIncreasingPower(x):
+    #code here
+    n = 1
+    num = 1
+    while(num <= x):
+        
+        print ( num, end = " ")
+        n+=1
+        num = n*n
+        
+        
