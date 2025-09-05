aa18de4fcc09238e1c0307eca10523f94930ab34
RM Muzammil <50529410+rm-muzammil@users.noreply.github.com>
2025-09-05 21:10:38 +0500

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/At least two greater elements/at-least-two-greater-elements.py
@@ -0,0 +1,10 @@
+#User function Template for python3
+
+class Solution:
+    def findElements(self,arr):
+        arr.sort()
+        arr.pop()
+        arr.pop()
+        return arr
+        
+    
