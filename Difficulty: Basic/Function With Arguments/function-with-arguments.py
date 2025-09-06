6f03be281df1bf682e65242fe23e98c6ec097511
RM Muzammil <50529410+rm-muzammil@users.noreply.github.com>
2025-09-06 18:08:58 +0500

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Function With Arguments/function-with-arguments.py
@@ -0,0 +1,10 @@
+#User function Template for python3
+
+#Write the complete argumentFunction below.
+#The function should take two arguments a and b
+#The function should return a+b
+#code here
+
+#Write the argumentFunction above.
+def argumentFunction(a,b):
+    return a + b
