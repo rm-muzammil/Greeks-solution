86c859ce3a575cc13db4aea54dc6be883050318b
RM Muzammil <50529410+rm-muzammil@users.noreply.github.com>
2025-10-04 22:31:13 +0500

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Alternates in an Array/alternates-in-an-array.cpp
@@ -0,0 +1,11 @@
+class Solution {
+  public:
+    vector<int> getAlternates(vector<int> &arr) {
+        vector<int> res;
+        
+        for(int i =0; i < arr.size();i+=2){
+            res.push_back(arr[i]);
+        }
+        return res;
+    }
+};
